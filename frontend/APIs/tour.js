6450c8367cdccebdb3f2be6e750b28c8ca406f02
KhoaNgex <khoawithexceptional@gmail.com>
2023-06-07 23:23:35 +0700

    add-feature

--- frontend/APIs/tour.js
+++ frontend/APIs/tour.js
@@ -1,3 +1,49 @@
+var tour_list;
+
+function searchAddress() {
+  var search_key = $("#inp").val();
+  if (search_key != "") {
+    var tourListHtml = "";
+    $.each(tour_list, function (index, tour) {
+      if (tour.TourAddress.toLowerCase().includes(search_key.toLowerCase())) {
+        console.log(tour);
+        tourListHtml =
+          tourListHtml +
+          `<figure>
+				<a href="rating.html?id=` +
+          tour.TID +
+          `"><img style="width: 100%; height: 220px;" src="image/tour` +
+          tour.TID +
+          `.jpg" alt='tourpic'></a>
+				<figcaption><strong class='title'>&lt;` +
+          tour.DayNum +
+          ` NGÀY - ` +
+          tour.TourName +
+          `&gt; </strong>` +
+          tour.TourDescription +
+          `</figcaption>
+				<div>
+					<span class='price'>VND <strong>` +
+          Number(tour.TourPrice).toLocaleString("en-US") +
+          `</strong> / ` +
+          tour.PeopleNum +
+          ` NGƯỜI</span>
+					<em class='sat'>CƠ HỘI TIẾT KIỆM ` +
+          tour.Discount +
+          `%</em>
+				</div>
+				<div class="type">
+					` +
+          tour.TourAddress +
+          `
+				</div>
+			</figure>`;
+      }
+    });
+    $("#tour-list").html(tourListHtml);
+  }
+}
+
 $(document).ready(function () {
   $.ajax({
     url: "http://localhost/TRV/backend/tour/getAll",
@@ -5,14 +51,14 @@ $(document).ready(function () {
     dataType: "json",
     success: function (tours) {
       try {
-        console.log(tours);
         // Xử lý kết quả trả về từ REST API
         var tourListHtml = "";
+        tour_list = tours;
         $.each(tours, function (index, tour) {
           tourListHtml =
             tourListHtml +
             `<figure>
-				<a href="tour-detail?id=` +
+				<a href="rating.html?id=` +
             tour.TID +
             `"><img style="width: 100%; height: 220px;" src="image/tour` +
             tour.TID +
